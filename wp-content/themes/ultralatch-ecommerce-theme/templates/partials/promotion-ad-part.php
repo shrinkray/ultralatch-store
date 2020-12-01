6c1942933ec427ec144840ecc5d76f555d5a681b
Greg <hello@gregmiller.io>
2020-12-01 13:07:27 -0500

    Update template file, remove inline styles

--- wp-content/themes/ultralatch-ecommerce-theme/templates/partials/promotion-ad-part.php
+++ wp-content/themes/ultralatch-ecommerce-theme/templates/partials/promotion-ad-part.php
@@ -8,24 +8,23 @@
  */
 ?>
 
-  <div class="container-fluid">
-  <div class="row">
+  <div class="ad-wrapper" >
+    <div class="row">
 
-    <div class="col col-sm-12">
-      <div class="ad-spot">
-        <div class="ad-door"></div>
-        <div class="ad-handle"></div>
-        <div class="ad-pitch">
-          <div class="headline">Antimicrobial Finish!</div>
-          <div class="pitch">Help eliminate the spread of viruses with our new antimicrobial
-            finishes on one of the most touched items in your daily life.</div>
-          <div class="pitch-cta glow"><a href="#" class="btn">Click to Learn More</a></div>
-
+      <div class="col col-sm-12">
+        <div class="ad-spot">
+          <div class="ad-door"></div>
+          <div class="ad-handle"></div>
+          <div class="ad-pitch">
+            <h2 class="headline">Antimicrobial Finish!</h2>
+            <div class="pitch">Help eliminate the spread of viruses with our new antimicrobial
+              finishes on one of the most touched items in your daily life.</div>
+            <div class="pitch-cta glow"><a href="#" title="Get this deal now!" class="btn">Click to Learn More</a></div>
+          </div>
         </div>
-
       </div>
+
     </div>
-  </div>
 </div>
 
 
