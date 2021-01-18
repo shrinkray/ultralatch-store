97533176797f824d3225c0c8134afad340727efa
Greg <hello@gregmiller.io>
2021-01-18 11:56:00 -0500

    Setup Laravel Mix files from Sapphire project

--- wp-content/themes/ultralatch-ecommerce-theme/postcss.config.js
+++ wp-content/themes/ultralatch-ecommerce-theme/postcss.config.js
@@ -1,6 +1,3 @@
-// n webpack you can use postcss-loader with autoprefixer and other PostCSS plugins.
 module.exports = {
-  plugins: [
-    require('autoprefixer')
-  ]
+    plugins: [require('postcss-preset-env')]
 }
---
+++ wp-content/themes/ultralatch-ecommerce-theme/webpack.mix.js
@@ -0,0 +1,60 @@
+/**
+ * @date 12-24-2020
+ * @author Greg
+ * TODO: Having problem with URL Processing
+ * Help may be here: https://github.com/postcss/postcss-url
+ * I think there may be better ways to handle the image URLs via Tailwinds and SCSS approach
+ * Because this test is an existing theme with preexisting file paths for code we are not even using
+ *   I dealt with the background images in elements.css .. Hopefully in a site built with SCSS,
+ *   URL processing will be effective
+ */
+
+let mix = require('laravel-mix');
+require('laravel-mix-postcss-config');
+let { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+mix
+    .js('assets/scripts/main.js', 'dist/js/main.js')
+    .options({
+        processCssUrls: false
+    })
+   // .css('style.css', 'dist/css/style.css')
+    .postCss('layout/css/app.css', 'dist/css/app.css', [
+        require('postcss-custom-properties'),
+        require('postcss-sorting')({
+            'properties-order': 'alphabetical'
+        }),
+        require('postcss-url')({
+         // Seeking options that work with Mix
+        }),
+        require('cssnano')
+    ])
+    .copyDirectory( [
+      { from: 'assets/images', to: 'dist/images' },
+      { from: 'assets/fonts', to: 'dist/fonts' }
+    ] )
+    .browserSync( {proxy: 'localhost:10068'})
+    .webpackConfig({
+        plugins: [
+            new CleanWebpackPlugin({
+                // Simulate the removal of files
+                // default: false
+                dry: false,
+
+                // Write Logs to Console
+                // (Always enabled when dry is true)
+                // default: false
+                verbose: false,
+
+                // Automatically remove all unused webpack assets on rebuild
+                // default: true
+                cleanStaleWebpackAssets: true,
+
+                // Do not allow removal of current webpack assets
+                // default: true
+                protectWebpackAssets: true,
+                cleanOnceBeforeBuildPatterns: ['dist/*', '!static-files*'],
+            })
+            ]
+         }
+    );
